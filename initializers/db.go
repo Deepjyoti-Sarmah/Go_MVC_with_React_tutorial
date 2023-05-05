91e4e518ffbe68b08b6a66ff45e2f691915c0a89
Deepjyoti-Sarmah <deepjyotisarmah37@gmail.com>
2023-05-05 15:47:24 +0530

    changed the code to make a react application with go

--- initializers/db.go
+++ initializers/db.go
@@ -2,20 +2,18 @@ package initializers
 
 import (
 	"fmt"
-	"os"
 
-	"github.com/DeepjyotiSarmah/go_MVC_with_React_tutorial/models"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
 var DB *gorm.DB
 
-func ConnectToDatabase() {
+func ConnectToDB() {
 	var err error
-	dsn := os.Getenv("DB_URL")
+	// dsn := os.Getenv("DB_URL")
 
-	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	DB, err = gorm.Open(postgres.Open("DB"), &gorm.Config{})
 
 	if err != nil {
 		fmt.Println("Failed to connect to database")
@@ -23,6 +21,6 @@ func ConnectToDatabase() {
 
 }
 
-func SyncDB() {
-	DB.AutoMigrate(&models.Post{}) //creates table in our database
-}
+// func SyncDB() {
+// 	DB.AutoMigrate(&models.Post{}) //creates table in our database
+// }
--- main.go
+++ main.go
@@ -3,32 +3,37 @@ package main
 import (
 	"os"
 
+	"github.com/DeepjyotiSarmah/go_MVC_with_React_tutorial/controllers"
 	"github.com/DeepjyotiSarmah/go_MVC_with_React_tutorial/initializers"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/template/html"
 )
 
 func init() {
-	initializers.LoadEnvVariable()
-	initializers.ConnectToDatabase()
-	initializers.SyncDB()
+	initializers.LoadEnvVars()
+	initializers.ConnectToDB()
 }
 
 func main() {
-	//Load templates
-	engine := html.New("./views", ".html")
+	// Load templates
+	engine := html.New("./views", ".tmpl")
 
-	//Setup app
+	// Create app
 	app := fiber.New(fiber.Config{
 		Views: engine,
 	})
 
-	//Config App
+	// Configure app
 	app.Static("/", "./public")
 
-	//Routes
-	Routes(app)
+	// Routing
+	app.Get("/api/tasks", controllers.FetchTasks)
+	app.Post("/api/tasks", controllers.CreateTask)
+	app.Get("/api/tasks/:id", controllers.FetchTask)
+	app.Delete("/api/tasks/:id", controllers.DeleteTask)
 
-	//Start app
+	app.Get("/", controllers.Home)
+
+	// Start app
 	app.Listen(":" + os.Getenv("PORT"))
 }
